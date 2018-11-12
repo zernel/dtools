0b8f92f0ba1c33df4a600ab9109a7f121fd59cbf
Doni Leong <doni.leong@gmail.com>
2018-11-12 10:11:48 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
 cat <<EOF > ~/.emacs
-(setq backup-directory-alist '(("" . "~/.emacs.d/emacs-backup")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
+ '(create-lockfiles nil)
+ '(make-backup-files nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 EOF
