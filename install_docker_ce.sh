25f28d710093045851ee093763ebe148574b9cc5
Doni Leong <doni.leong@gmail.com>
2019-01-21 20:23:40 +0800

    Update install_docker_ce.sh

--- install_docker_ce.sh
+++ install_docker_ce.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/bash
 
-sudo yum install -y yum-utils
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
 sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum makecache fast
-sudo yum update -y
+
 
 sudo yum install docker-ce -y
 
 sudo systemctl start docker
 sudo systemctl enable docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
