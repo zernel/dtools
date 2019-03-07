164874120f5ba7bc95712fa18fcb8aac5320693b
Zernel <itzernel@gmail.com>
2019-03-07 08:43:37 +0800

    Update install_docker_ce.sh

--- install_docker_ce.sh
+++ install_docker_ce.sh
@@ -16,3 +16,5 @@ sudo systemctl enable docker
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+service docker restart
